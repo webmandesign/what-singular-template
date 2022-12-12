f2c1f520ddfcb9ce83479d3e6f1b96afd27d0f99
WebMan Design, Oliver Juhas <webmandesigneu@gmail.com>
2022-12-12 18:06:42 +0100

    1.0.0

---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== What Singular Template ===
+
+Contributors:      webmandesign
+Donate link:       https://www.webmandesign.eu/contact/#donation
+Author URI:        https://www.webmandesign.eu
+Plugin URI:        https://www.webmandesign.eu/portfolio/what-singular-template-wordpress-plugin/
+Requires at least: 5.9
+Tested up to:      6.1
+Requires PHP:      7.0
+Stable tag:        1.0.0
+License:           GNU General Public License v3
+License URI:       http://www.gnu.org/licenses/gpl-3.0.html
+Tags:              webman, webman design, template, page template, post template, reveal, show, display
+
+Displays name of template assigned to a post, page or public custom post type in admin posts list table.
+
+
+== Description ==
+
+Displays name of template assigned to a post, page or public custom post type in admin posts list table.
+
+
+== Installation ==
+
+1. Unzip the plugin download file and upload `what-singular-template` folder into the `/wp-content/plugins/` directory.
+2. Activate the plugin through the *"Plugins"* menu in WordPress.
+3. Plugin works immediately after activation by adding additional column to page/post/custom post type list table in WordPress admin.
+
+
+== Frequently Asked Questions ==
+
+= How does it work? =
+
+Once you enable the plugin, it adds new "Template" column in posts list table in WordPress admin area. Simply navigate to "Posts" or "Pages" admin screen to preview.
+
+
+== Screenshots ==
+
+1. Posts list table with "Template" column.
+
+
+== Changelog ==
+
+Please see the [`changelog.md` file](https://github.com/webmandesign/what-singular-template/blob/master/changelog.md) for details.
+
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+Initial release.
---
+++ what-singular-template.php
@@ -0,0 +1,213 @@
+<?php
+/**
+ * Plugin Name:  What Singular Template
+ * Plugin URI:   https://www.webmandesign.eu/portfolio/what-singular-template-wordpress-plugin/
+ * Description:  Displays name of template assigned to a post, page or public custom post type in admin posts list table.
+ * Version:      1.0.0
+ * Author:       WebMan Design, Oliver Juhas
+ * Author URI:   https://www.webmandesign.eu/
+ * License:      GNU General Public License v3
+ * License URI:  http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:  what-singular-template
+ * Domain Path:  /languages
+ *
+ * GitHub Plugin URI:  https://github.com/webmandesign/what-singular-template
+ *
+ * @copyright  WebMan Design, Oliver Juhas
+ * @license    GPL-3.0, https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @link  https://github.com/webmandesign/what-singular-template
+ * @link  https://www.webmandesign.eu
+ *
+ * @package  What Singular Template
+ */
+
+namespace WebManDesign\WST;
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Plugin functionality class.
+ */
+class Load {
+
+	/**
+	 * ID/name of templates column.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 * @var     string
+	 */
+	public static $column_name = 'wst_template';
+
+	/**
+	 * List of template paths with their names.
+	 *
+	 * @since   1.0.0
+	 * @access  public
+	 * @var     array
+	 */
+	public static $templates = array();
+
+	/**
+	 * Initialization.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @return  void
+	 */
+	public static function init() {
+
+		// Requirements check
+
+			if ( ! is_admin() ) {
+				return;
+			}
+
+
+		// Variables
+
+			self::$templates  = wp_get_theme()->get_page_templates();
+
+			$post_types = get_post_types( array( 'public' => true ) );
+
+			unset( $post_types['attachment'] );
+
+
+		// Processing
+
+			// Actions
+
+				add_action( 'manage_pages_columns', __CLASS__ . '::columns' );
+				add_action( 'manage_posts_columns', __CLASS__ . '::columns' );
+
+				add_action( 'manage_pages_custom_column', __CLASS__ . '::content', 10, 2 );
+				add_action( 'manage_posts_custom_column', __CLASS__ . '::content', 10, 2 );
+
+			// Filters
+
+				foreach ( $post_types as $post_type ) {
+					add_filter( 'manage_edit-' . $post_type . '_sortable_columns', __CLASS__ . '::register_sortable' );
+				}
+
+				add_filter( 'request', __CLASS__ . '::do_sort' );
+
+	} // /init
+
+	/**
+	 * Add new columns to posts list table.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @param array  $post_columns  An associative array of column headings.
+	 *
+	 * @return  array
+	 */
+	public static function columns( array $post_columns ): array {
+
+		// Processing
+
+			$post_columns[ self::$column_name ] = esc_html_x( 'Template', 'Page/post template', 'what-singular-template' );
+
+
+		// Output
+
+			return $post_columns;
+
+	} // /columns
+
+	/**
+	 * Outputs posts list table column content.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @param string $column_name  The name of the column to display.
+	 * @param int    $post_id      The current post ID.
+	 *
+	 * @return  void
+	 */
+	public static function content( string $column_name, int $post_id ) {
+
+		// Requirements check
+
+			if ( self::$column_name !== $column_name ) {
+				return;
+			}
+
+
+		// Variables
+
+			$template = get_post_meta( $post_id, '_wp_page_template', true );
+
+
+		// Output
+
+			if (
+				$template
+				&& 'default' !== $template
+				&& isset( self::$templates[ $template ] )
+			) {
+				echo '<strong title="' . esc_attr_x( 'Template file: ', 'Page/post template', 'what-singular-template' ) . esc_attr( $template ) . '">' . esc_html( self::$templates[ $template ] ) . '</strong>';
+			} else {
+				echo '<em>' . esc_html_x( 'Default template', 'Page/post template', 'what-singular-template' ) . '</em>';
+			}
+
+	} // /content
+
+	/**
+	 * Register posts list table column as sortable.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @param array $sortable_columns An array of sortable columns.
+	 *
+	 * @return  array
+	 */
+	public static function register_sortable( array $sortable_columns ): array {
+
+		// Processing
+
+			$sortable_columns[ self::$column_name ] = self::$column_name;
+
+
+		// Output
+
+			return $sortable_columns;
+
+	} // /register_sortable
+
+	/**
+	 * Handle the query variables for sorting.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @param array $query_vars  The array of requested query variables.
+	 *
+	 * @return  array
+	 */
+	public static function do_sort( array $query_vars ): array {
+
+		// Processing
+
+			if (
+				isset( $query_vars['orderby'] )
+				&& self::$column_name === $query_vars['orderby']
+			) {
+
+				$query_vars = array_merge( $query_vars, array(
+					'meta_key' => '_wp_page_template',
+					'orderby'  => 'meta_value',
+				) );
+			}
+
+
+ 		// Output
+
+			return $query_vars;
+
+	} // /do_sort
+
+}
+
+add_action( 'admin_init', 'WebManDesign\WST\Load::init', 101 );
